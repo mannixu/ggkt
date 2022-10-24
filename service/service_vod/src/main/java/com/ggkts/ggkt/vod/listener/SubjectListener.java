86599a7b21bbce2ba0b02051763d888fdc1f4885
mannixu <1730365363@qq.com>
2022-10-24 22:11:56 +0800

    课程分类与excel文件的上传与下载

---
+++ service/service_vod/src/main/java/com/ggkts/ggkt/vod/listener/SubjectListener.java
@@ -0,0 +1,31 @@
+package com.ggkts.ggkt.vod.listener;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.ggkts.ggkt.model.vod.Subject;
+import com.ggkts.ggkt.vo.vod.SubjectEeVo;
+import com.ggkts.ggkt.vod.mapper.SubjectMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class SubjectListener extends AnalysisEventListener<SubjectEeVo> {
+
+    @Autowired
+    private SubjectMapper subjectMapper;
+
+
+    @Override
+    public void invoke(SubjectEeVo subjectEeVo, AnalysisContext analysisContext) {
+        Subject subject = new Subject();
+        BeanUtils.copyProperties(subjectEeVo,subject);
+        subjectMapper.insert(subject);
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+
+    }
+}
---
+++ service/service_vod/src/main/java/com/ggkts/ggkt/vod/mapper/SubjectMapper.java
@@ -0,0 +1,18 @@
+package com.ggkts.ggkt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ggkts.ggkt.model.vod.Subject;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 课程科目 Mapper 接口
+ * </p>
+ *
+ * @author atguigu
+ * @since 2022-04-21
+ */
+@Repository
+public interface SubjectMapper extends BaseMapper<Subject> {
+
+}
